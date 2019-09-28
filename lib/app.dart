1a3209a11d1dc57114d946e18d14249d6a9e36f2
mayhew3 <commander.boffo@gmail.com>
2019-09-27 18:29:45 -0700

    TM-9: Failing attempt to add date picker

--- lib/app.dart
+++ lib/app.dart
@@ -1,4 +1,3 @@
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:taskmaster/models.dart';
 import 'package:taskmaster/screens/home_screen.dart';
--- lib/screens/detail_screen.dart
+++ lib/screens/detail_screen.dart
@@ -3,7 +3,9 @@ import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:taskmaster/models.dart';
 
-class DetailScreen extends StatelessWidget {
+import 'package:taskmaster/widgets/date_time_picker.dart';
+
+class DetailScreen extends StatefulWidget {
   final TaskItem taskItem;
 
   const DetailScreen({
@@ -11,6 +13,17 @@ class DetailScreen extends StatelessWidget {
     @required this.taskItem
   }) : super(key: key);
 
+}
+
+class DetailScreenState extends State<DetailScreen> {
+
+  final TaskItem taskItem;
+  
+  const DetailScreenState({
+    Key key, 
+    @required this.taskItem
+  }) : super(key: key);
+  
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -38,6 +51,21 @@ class DetailScreen extends StatelessWidget {
                   ),
                   initialValue: taskItem.description,
                   style: Theme.of(context).textTheme.display1,
+                ),
+                DateTimePicker(
+                  labelText: 'Start',
+                  selectedDate: taskItem.startDate,
+                  selectedTime: taskItem.startDate == null ? null : TimeOfDay(hour: taskItem.startDate.hour, minute: taskItem.startDate.minute),
+                  selectDate: (DateTime date) {
+                    setState(() {
+                      _fromDate = date;
+                    });
+                  },
+                  selectTime: (TimeOfDay time) {
+                    setState(() {
+                      _fromTime = time;
+                    });
+                  },
                 ),
                 Text(
                   taskItem.dateAdded.toString(),
