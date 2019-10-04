304e422eb5e12a6ca65e2db99ab5c56cc23b80ac
Mayhew Seavey <commander.boffo@gmail.com>
2019-10-04 11:23:45 -0700

    TM-15: Replace old date time usage with new field. Move new field to separate file.

--- lib/screens/detail_screen.dart
+++ lib/screens/detail_screen.dart
@@ -1,11 +1,9 @@
 
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
-import 'package:intl/intl.dart';
 import 'package:taskmaster/models.dart';
 import 'package:taskmaster/typedefs.dart';
-import 'package:datetime_picker_formfield/datetime_picker_formfield.dart';
-import 'package:taskmaster/widgets/date_time_picker.dart';
+import 'package:taskmaster/widgets/clearable_date_time_field.dart';
 
 class DetailScreen extends StatefulWidget {
   final TaskItem taskItem;
@@ -21,12 +19,6 @@ class DetailScreen extends StatefulWidget {
   State<StatefulWidget> createState() => DetailScreenState();
 }
 
-DateTime daysFromNow(int days) {
-  DateTime now = DateTime.now();
-  DateTime atSeven = new DateTime(now.year, now.month, now.day, 19);
-  return atSeven.add(new Duration(days: days));
-}
-
 class DetailScreenState extends State<DetailScreen> {
   static final GlobalKey<FormState> formKey = GlobalKey<FormState>();
 
@@ -38,6 +30,7 @@ class DetailScreenState extends State<DetailScreen> {
   @override
   void initState() {
     super.initState();
+    _startDate = widget.taskItem.startDate;
     _targetDate = widget.taskItem.targetDate;
   }
 
@@ -75,13 +68,9 @@ class DetailScreenState extends State<DetailScreen> {
                   initialValue: widget.taskItem?.description,
                   onSaved: (value) => _description = value,
                 ),
-                DateTimePicker(
+                ClearableDateTimeField(
                   labelText: 'Start',
-                  defaultDate: daysFromNow(7),
                   dateGetter: () {
-                    if (_startDate == null) {
-                      _startDate = widget.taskItem.startDate;
-                    }
                     return _startDate;
                   },
                   dateSetter: (DateTime pickedDate) {
@@ -90,7 +79,7 @@ class DetailScreenState extends State<DetailScreen> {
                     });
                   },
                 ),
-                BasicDateTimeField(
+                ClearableDateTimeField(
                   labelText: 'Target',
                   dateGetter: () {
                     return _targetDate;
@@ -131,50 +120,4 @@ class DetailScreenState extends State<DetailScreen> {
   }
 
   bool get isEditing => widget.taskItem != null;
-}
-
-final longDateFormat = DateFormat.yMMMMd().add_jm();
-
-class BasicDateTimeField extends StatelessWidget {
-  const BasicDateTimeField({
-    Key key,
-    this.labelText,
-    this.dateGetter,
-    this.dateSetter,
-  }) : super(key: key);
-
-  final String labelText;
-  final ValueGetter<DateTime> dateGetter;
-  final ValueChanged<DateTime> dateSetter;
-
-  @override
-  Widget build(BuildContext context) {
-    return InputDecorator(
-      decoration: InputDecoration(
-        labelText: labelText,
-      ),
-      child: DateTimeField(
-        format: longDateFormat,
-        initialValue: dateGetter(),
-        onChanged: (pickedDate) => dateSetter(pickedDate),
-        onShowPicker: (context, currentValue) async {
-          final date = await showDatePicker(
-              context: context,
-              firstDate: DateTime(1900),
-              initialDate: currentValue ?? daysFromNow(7),
-              lastDate: DateTime(2100));
-          if (date != null) {
-            final time = await showTimePicker(
-              context: context,
-              initialTime:
-              TimeOfDay.fromDateTime(currentValue ?? daysFromNow(7)),
-            );
-            return DateTimeField.combine(date, time);
-          } else {
-            return currentValue;
-          }
-        },
-      ),
-    );
-  }
 }
---
+++ lib/widgets/clearable_date_time_field.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+import 'package:datetime_picker_formfield/datetime_picker_formfield.dart';
+
+final longDateFormat = DateFormat.yMMMMd().add_jm();
+
+DateTime daysFromNow(int days) {
+  DateTime now = DateTime.now();
+  DateTime atSeven = new DateTime(now.year, now.month, now.day, 19);
+  return atSeven.add(new Duration(days: days));
+}
+
+class ClearableDateTimeField extends StatelessWidget {
+  const ClearableDateTimeField({
+    Key key,
+    this.labelText,
+    this.dateGetter,
+    this.dateSetter,
+  }) : super(key: key);
+
+  final String labelText;
+  final ValueGetter<DateTime> dateGetter;
+  final ValueChanged<DateTime> dateSetter;
+
+  @override
+  Widget build(BuildContext context) {
+    return InputDecorator(
+      decoration: InputDecoration(
+        labelText: labelText,
+      ),
+      child: DateTimeField(
+        format: longDateFormat,
+        initialValue: dateGetter(),
+        onChanged: (pickedDate) => dateSetter(pickedDate),
+        onShowPicker: (context, currentValue) async {
+          final date = await showDatePicker(
+              context: context,
+              firstDate: DateTime(1900),
+              initialDate: currentValue ?? daysFromNow(7),
+              lastDate: DateTime(2100));
+          if (date != null) {
+            final time = await showTimePicker(
+              context: context,
+              initialTime:
+              TimeOfDay.fromDateTime(currentValue ?? daysFromNow(7)),
+            );
+            return DateTimeField.combine(date, time);
+          } else {
+            return currentValue;
+          }
+        },
+      ),
+    );
+  }
+}
