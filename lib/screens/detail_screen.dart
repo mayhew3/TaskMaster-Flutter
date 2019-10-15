1a1da3e57d68f1a2fa5fcacf2ccfaab82142bfb0
Mayhew Seavey <commander.boffo@gmail.com>
2019-10-14 20:20:13 -0700

    TM-34: Add other text fields

--- lib/screens/detail_screen.dart
+++ lib/screens/detail_screen.dart
@@ -3,8 +3,11 @@ import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:taskmaster/models.dart';
 import 'package:taskmaster/typedefs.dart';
+import 'package:intl/intl.dart';
 import 'package:taskmaster/screens/add_edit_screen.dart';
 import 'package:taskmaster/widgets/readonly_task_field.dart';
+
+final longDateFormat = DateFormat.yMMMMd().add_jm();
 
 class DetailScreen extends StatelessWidget {
   final TaskItem taskItem;
@@ -17,6 +20,10 @@ class DetailScreen extends StatelessWidget {
     this.taskAdder,
     this.taskUpdater,
   }) : super(key: key);
+
+  String formatDateTime(DateTime dateTime) {
+    return dateTime == null ? '' : longDateFormat.format(dateTime);
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -46,6 +53,30 @@ class DetailScreen extends StatelessWidget {
                   ),
                 ),
               ],
+            ),
+            ReadOnlyTaskField(
+              headerName: 'Project',
+              textToShow: taskItem.project,
+            ),
+            ReadOnlyTaskField(
+              headerName: 'Context',
+              textToShow: taskItem.context,
+            ),
+            ReadOnlyTaskField(
+              headerName: 'Start',
+              textToShow: formatDateTime(taskItem.startDate),
+            ),
+            ReadOnlyTaskField(
+              headerName: 'Urgent',
+              textToShow: formatDateTime(taskItem.urgentDate),
+            ),
+            ReadOnlyTaskField(
+              headerName: 'Target',
+              textToShow: formatDateTime(taskItem.targetDate),
+            ),
+            ReadOnlyTaskField(
+              headerName: 'Due',
+              textToShow: formatDateTime(taskItem.dueDate),
             ),
             ReadOnlyTaskField(
               headerName: 'Notes',
