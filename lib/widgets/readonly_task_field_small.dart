fda4de91008d602e525ea000dd79def174e8c1fb
mayhew3 <commander.boffo@gmail.com>
2019-10-19 18:22:31 -0700

    TM-57: Put small readonlys in cards

--- lib/widgets/readonly_task_field_small.dart
+++ lib/widgets/readonly_task_field_small.dart
@@ -1,6 +1,7 @@
 
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:taskmaster/models/task_colors.dart';
 
 class ReadOnlyTaskFieldSmall extends StatelessWidget {
   final String headerName;
@@ -16,15 +17,21 @@ class ReadOnlyTaskFieldSmall extends StatelessWidget {
   Widget build(BuildContext context) {
     return Visibility(
       visible: textToShow.isNotEmpty,
-      child: Padding(
-        padding: EdgeInsets.all(16.0),
-        child: Column(
-          children: <Widget>[
-            Text(headerName,
-                style: Theme.of(context).textTheme.caption,),
-            Text(textToShow,
-              style: Theme.of(context).textTheme.subhead,),
-          ],
+      child: Expanded(
+        child: Card(
+          elevation: 3.0,
+          color: TaskColors.cardColor,
+          child: Padding(
+            padding: EdgeInsets.symmetric(vertical: 12.0),
+            child: Column(
+              children: <Widget>[
+                Text(headerName,
+                  style: Theme.of(context).textTheme.caption,),
+                Text(textToShow,
+                  style: Theme.of(context).textTheme.subhead,),
+              ],
+            ),
+          ),
         ),
       ),
     );
