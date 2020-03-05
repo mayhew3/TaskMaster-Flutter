05fd2f04b0da474def802e482163385944151350
mayhew3 <commander.boffo@gmail.com>
2020-03-05 13:22:49 -0800

    TM-80: Move simple mock to shared file

--- test/mocks/mock_app_state.dart
+++ test/mocks/mock_app_state.dart
@@ -6,7 +6,6 @@ import 'package:taskmaster/models/app_state.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:taskmaster/models/task_item.dart';
 
-import 'mock_google_sign_in_account.dart';
 import 'mock_id_token_result.dart';
 
 class MockAppState extends Mock implements AppState {
@@ -29,3 +28,6 @@ class MockAppState extends Mock implements AppState {
     return Future<IdTokenResult>.value(idTokenResult);
   }
 }
+
+
+class MockGoogleSignInAccount extends Mock implements GoogleSignInAccount {}
--- test/mocks/mock_google_sign_in_account.dart
+++
@@ -1,6 +0,0 @@
-import 'package:google_sign_in/google_sign_in.dart';
-import 'package:mockito/mockito.dart';
-
-class MockGoogleSignInAccount extends Mock implements GoogleSignInAccount {
-  String get email => 'scorpy@gmail.com';
-}
