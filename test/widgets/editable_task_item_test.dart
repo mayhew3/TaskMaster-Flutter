7350b7f9ed4a5a8e005cd8f3b519b5cd816ce7f6
mayhew3 <commander.boffo@gmail.com>
2020-07-06 18:53:36 -0700

    Update dependencies to make tests work

---
+++ test/widgets/editable_task_item_test.dart
@@ -0,0 +1,27 @@
+
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:taskmaster/widgets/editable_task_field.dart';
+
+void main() {
+
+  Future<MaterialApp> _createApp(WidgetTester tester, {bool isRequired, FormFieldValidator<String> validator}) async {
+
+    var app = MaterialApp(
+      home: Scaffold(
+      ),
+    );
+    await tester.pumpWidget(app);
+    return app;
+  }
+
+  testWidgets('has label', (WidgetTester tester) async {
+    await _createApp(tester);
+
+    var labelFinder = find.text('Type');
+
+    expect(labelFinder, findsOneWidget);
+
+  });
+
+}
