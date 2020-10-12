97f67d86fda89e9b4ff716cd787a8e3e665d249c
mayhew3 <commander.boffo@gmail.com>
2020-10-11 17:55:13 -0700

    TM-102: Fix test compile

--- test/mocks/mock_task_helper.dart
+++ test/mocks/mock_task_helper.dart
@@ -1,11 +1,9 @@
 import 'package:flutter/src/widgets/framework.dart';
+import 'package:mockito/mockito.dart';
 import 'package:taskmaster/models/app_state.dart';
 import 'package:taskmaster/models/task_item.dart';
-import 'package:taskmaster/nav_helper.dart';
 import 'package:taskmaster/task_helper.dart';
 import 'package:taskmaster/task_repository.dart';
-import 'package:taskmaster/typedefs.dart';
-import 'package:mockito/mockito.dart';
 
 class MockTaskHelper extends Mock implements TaskHelper {
 
@@ -26,7 +24,7 @@ class MockTaskHelper extends Mock implements TaskHelper {
   }
 
   @override
-  Future<TaskItem> completeTask(TaskItem taskItem, bool completed) async {
+  Future<TaskItem> completeTask(TaskItem taskItem, bool completed, stateSetter) async {
     return Future.value(taskItem);
   }
 
