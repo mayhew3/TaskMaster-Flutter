b81d00ca17219f1ce3f5c144ce208bd757e12815
Mayhew Seavey <commander.boffo@gmail.com>
2024-02-15 19:54:21 -0800

    Redux: add more to app state

--- lib/redux/redux_app_state.dart
+++ lib/redux/redux_app_state.dart
@@ -2,28 +2,39 @@ import 'package:meta/meta.dart';
 import 'package:taskmaster/models/task_item.dart';
 
 import '../models/app_tab.dart';
+import '../models/sprint.dart';
+import '../models/task_recurrence.dart';
 
 @immutable
 class ReduxAppState {
   final bool isLoading;
   final List<TaskItem> taskItems;
+  final List<Sprint> sprints;
+  final List<TaskRecurrence> taskRecurrences;
   final AppTab activeTab;
 
-  ReduxAppState(
-      {this.isLoading = false,
-        this.taskItems = const [],
-        this.activeTab = AppTab.plan});
+  ReduxAppState({
+    this.isLoading = false,
+    this.taskItems = const [],
+    this.sprints = const [],
+    this.taskRecurrences = const [],
+    this.activeTab = AppTab.plan
+  });
 
   factory ReduxAppState.loading() => ReduxAppState(isLoading: true);
 
   ReduxAppState copyWith({
     bool? isLoading,
     List<TaskItem>? todos,
+    List<Sprint>? sprints,
+    List<TaskRecurrence>? taskRecurrences,
     AppTab? activeTab,
   }) {
     return ReduxAppState(
       isLoading: isLoading ?? this.isLoading,
       taskItems: todos ?? this.taskItems,
+      sprints: sprints ?? this.sprints,
+      taskRecurrences: taskRecurrences ?? this.taskRecurrences,
       activeTab: activeTab ?? this.activeTab,
     );
   }
@@ -41,10 +52,12 @@ class ReduxAppState {
               runtimeType == other.runtimeType &&
               isLoading == other.isLoading &&
               taskItems == other.taskItems &&
+              sprints == other.sprints &&
+              taskRecurrences == other.taskRecurrences &&
               activeTab == other.activeTab;
 
   @override
   String toString() {
-    return 'AppState{isLoading: $isLoading, todos: $taskItems, activeTab: $activeTab}';
+    return 'AppState{isLoading: $isLoading, taskItems: $taskItems, sprints: $sprints, recurrences: $taskRecurrences, activeTab: $activeTab}';
   }
 }
