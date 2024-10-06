2e41b2c130f48b0a7784a773a9c21205885b09df
Mayhew Park <commander.boffo@gmail.com>
2024-10-05 17:33:51 -0700

    TM-252: Fix more tests

--- lib/models/snooze.dart
+++ lib/models/snooze.dart
@@ -1,5 +1,6 @@
 import 'package:built_value/built_value.dart';
 import 'package:built_value/serializer.dart';
+import 'package:taskmaster/models/serializers.dart';
 
 part 'snooze.g.dart';
 
@@ -19,4 +20,12 @@ abstract class Snooze implements Built<Snooze, SnoozeBuilder> {
 
   Snooze._();
   factory Snooze([Function(SnoozeBuilder) updates]) = _$Snooze;
+
+  dynamic toJson() {
+    return serializers.serializeWith(Snooze.serializer, this);
+  }
+
+  static Snooze fromJson(dynamic json) {
+    return serializers.deserializeWith(Snooze.serializer, json)!;
+  }
 }
--- test/snooze_test.dart
+++ test/snooze_test.dart
@@ -1,44 +1,47 @@
 
+import 'package:taskmaster/models/snooze.dart';
 import 'package:test/test.dart';
 
 void main() {
 
   test('to json', () async {
-    Snooze snooze = new Snooze(
-        taskId: 4,
-        snoozeNumber: 1,
-        snoozeUnits: "days",
-        snoozeAnchor: "Due",
-        newAnchor: DateTime(2023, 2, 1));
+    Snooze snooze = new Snooze((s) => s
+      ..id = 3
+      ..dateAdded = DateTime.now().toUtc()
+      ..taskId = 4
+      ..snoozeNumber = 1
+      ..snoozeUnits = "days"
+      ..snoozeAnchor = "Due"
+      ..newAnchor = DateTime.utc(2023, 2, 1));
 
     Map<String, dynamic> json = snooze.toJson();
-    expect(json.length, 5);
-    expect(json['task_id'], 4);
-    expect(json['snooze_number'], 1);
-    expect(json['snooze_units'], "days");
-    expect(json['snooze_anchor'], "Due");
-    expect(json['new_anchor'], "2023-02-01T00:00:00.000");
+    expect(json.length, 8);
+    expect(json['taskId'], 4);
+    expect(json['snoozeNumber'], 1);
+    expect(json['snoozeUnits'], "days");
+    expect(json['snoozeAnchor'], "Due");
+    expect(json['newAnchor'], "2023-02-01T00:00:00.000Z");
   });
 
   test('from json', () async {
     Map<String, dynamic> json = {
       "id": 2302,
-      "date_added": "2023-01-01T00:00:00.000",
-      "task_id": 4,
-      "snooze_number": 1,
-      "snooze_units": "days",
-      "snooze_anchor": "Due",
-      "new_anchor": "2023-02-01T00:00:00.000"
+      "dateAdded": "2023-01-01T00:00:00.000Z",
+      "taskId": 4,
+      "snoozeNumber": 1,
+      "snoozeUnits": "days",
+      "snoozeAnchor": "Due",
+      "newAnchor": "2023-02-01T00:00:00.000Z"
     };
 
     Snooze snooze = Snooze.fromJson(json);
     expect(snooze.id, 2302);
-    expect(snooze.dateAdded, DateTime(2023, 1, 1));
+    expect(snooze.dateAdded, DateTime.utc(2023, 1, 1));
     expect(snooze.taskId, 4);
     expect(snooze.snoozeNumber, 1);
     expect(snooze.snoozeUnits, "days");
     expect(snooze.snoozeAnchor, "Due");
-    expect(snooze.newAnchor, DateTime(2023, 2, 1));
+    expect(snooze.newAnchor, DateTime.utc(2023, 2, 1));
   });
 
 }
