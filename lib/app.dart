2606baf83c9187a4551925581413ee5f14d3f8b5
Mayhew Park <commander.boffo@gmail.com>
2024-10-27 16:43:00 -0700

    TM-255: Env variable for firestore

--- lib/app.dart
+++ lib/app.dart
@@ -32,18 +32,21 @@ class TaskMasterAppState extends State<TaskMasterApp> {
   late final Store<AppState> store;
   static final _navigatorKey = GlobalKey<NavigatorState>();
 
+  static const serverEnv = String.fromEnvironment('SERVER', defaultValue: 'heroku');
+
   @override
   void initState() {
     super.initState();
     var firestore = FirebaseFirestore.instance;
-    firestore.settings = const Settings(cacheSizeBytes: Settings.CACHE_SIZE_UNLIMITED);
 
-    // LOCAL EMULATOR SETTINGS - USE WITH ENV VARIABLE
-
-    firestore.useFirestoreEmulator("127.0.0.1", 8085);
-    firestore.settings = const Settings(
-      persistenceEnabled: false,
-    );
+    if (serverEnv == 'local') {
+      firestore.useFirestoreEmulator("127.0.0.1", 8085);
+      firestore.settings = const Settings(
+        persistenceEnabled: false,
+      );
+    } else {
+      firestore.settings = const Settings(cacheSizeBytes: Settings.CACHE_SIZE_UNLIMITED);
+    }
 
     var taskRepository = TaskRepository(client: http.Client(), firestore: firestore);
     store = Store<AppState>(
